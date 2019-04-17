bcf1f15d2c6c8e2b545596d727d89163898aec7e
Ting <wisezt@hotmail.com>
2019-04-17 10:07:20 -0500

    Created a person method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
        <h1>Hi, I'am  a React App</h1>
 	   <p>This is working</p>
+          <Person />
       </div>)
 	  
   }
