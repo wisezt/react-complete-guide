823d8d82bcaa2187673c3d506d51f97c83fad60b
Ting <wisezt@hotmail.com>
2019-04-18 08:21:51 -0500

    bad one

--- src/App.js
+++ src/App.js
@@ -7,20 +7,14 @@ class App extends Component {
   render() {
 	  
 	  
-    return (
+    return
       <div className="App">
        <h1>Hi, I'am  a React App</h1>
 	   <p>This is working</p>
-          <Person />
-          <Person />
-          <Person />
-          <Person />
-      </div>)
-	  
-  }
-  //	  return React.createElement('div', {className: 'App'}, '', React.createElement('h1', null, 'Is it works?'))}
-    
-  
-}
+      </div>
+
+
+};
+};
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () =>{
-    return <p>I' m a Person!</p>
+const person = ( ) =>{
+    return
+
+        <p>I' m  and I am {Math.floor(Math.random()*30)}  years old!</p>
+
+
+
 
 
 };
