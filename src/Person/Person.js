6840221d3d9d1c4d53a1529fbd37faa001ef758a
Ting <wisezt@hotmail.com>
2019-04-18 14:34:27 -0500

    added a Person.css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 
 const person = (props) =>{
 	return (
-		<div>
+		<div className= "Person">
 			<p > I'm {props.name}. and {props.age} years old </p>
 		    <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
